113bc336a35013f0eac88b33a55e1afb8457e2de
jujinyoung <jujinyoung1838@naver.com>
2024-01-01 14:48:49 +0900

    [Ju] 2667 외판원 - 미풀이

---
+++ ju/week14/_2098_not.java
@@ -0,0 +1,21 @@
+package ju.week14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 외판원 순회
+ *
+ * 문제 이해가 안됨
+ */
+public class _2098_not {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+    }
+}
