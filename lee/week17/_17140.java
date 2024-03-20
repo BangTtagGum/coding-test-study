892b1e7ee431c424ff115270964f7a78749dde94
Junyoung Lee <99023627+BangTtagGum@users.noreply.github.com>
2024-03-20 17:39:09 +0900

    [Jun] week17 _17140 코드 정리

--- lee/week17/_17140.java
+++ lee/week17/_17140.java
@@ -7,19 +7,19 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class _17140 {
+
     public static int[][] array = new int[101][101];
     public static int maxRow = 3;
     public static int maxColumn = 3;
+
     public static void R() {
         int size = maxColumn;
         int newMaxColumn = 0;
         for (int i = 1; i <= maxRow; i++) {
-
             Map<Integer, Integer> count = new LinkedHashMap<>();
 
             for (int j = 1; j <= size; j++) {
@@ -29,8 +29,7 @@ public class _17140 {
             }
 
             // 맵 정렬
-            Set<Entry<Integer, Integer>> entries = count.entrySet();
-            List<Entry<Integer, Integer>> collect = entries.stream().sorted((m1, m2) -> {
+            List<Entry<Integer, Integer>> collect = count.entrySet().stream().sorted((m1, m2) -> {
                 if (m1.getValue() == m2.getValue()) {
                     return m1.getKey() - m2.getKey();
                 }
@@ -52,15 +51,17 @@ public class _17140 {
 
             // 배열이 짧아질 경우 뒤의 숫자들 제거
             if (idx <= maxColumn) {
-                for (int j = idx; j <= maxColumn ; j++) {
+                for (int j = idx; j <= maxColumn; j++) {
                     array[i][j] = 0;
                 }
             }
         }
         maxColumn = Math.min(newMaxColumn, 100);
     }
+
     public static void C() {
         int size = maxRow;
+        int newMaxRow = 0;
 
         for (int i = 1; i <= maxColumn; i++) {
             Map<Integer, Integer> count = new LinkedHashMap<>();
@@ -72,19 +73,17 @@ public class _17140 {
             }
 
             // 맵 정렬
-            Set<Entry<Integer, Integer>> entries = count.entrySet();
-            List<Entry<Integer, Integer>> collect = entries.stream().sorted((m1, m2) -> {
+            List<Entry<Integer, Integer>> collect = count.entrySet().stream().sorted((m1, m2) -> {
                 if (m1.getValue() == m2.getValue()) {
                     return m1.getKey() - m2.getKey();
                 }
                 return m1.getValue() - m2.getValue();
             }).collect(Collectors.toList());
 
+            // 가장 긴 Row 길이 저장
+            newMaxRow = Math.max(newMaxRow, collect.size() * 2);
+
             // 배열 재정렬
-            maxRow = Math.max(maxRow, collect.size() * 2);
-            if (maxRow > 100) {
-                maxRow = 100;
-            }
             int idx = 1;
             for (Entry<Integer, Integer> entry : collect) {
                 if (idx > 100) {
@@ -94,11 +93,12 @@ public class _17140 {
                 array[idx++][i] = entry.getValue();
             }
             if (idx <= maxRow) {
-                for (int j = idx; j <= maxRow ; j++) {
+                for (int j = idx; j <= maxRow; j++) {
                     array[j][i] = 0;
                 }
             }
         }
+        maxRow = Math.min(newMaxRow, 100);
     }
 
     public static void main(String[] args) throws IOException {
@@ -110,7 +110,6 @@ public class _17140 {
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
 
         for (int i = 1; i <= 3; i++) {
             st = new StringTokenizer(br.readLine());
